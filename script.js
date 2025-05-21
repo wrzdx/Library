62e06dff5f07554389d21ad5c06840f4fe760fd3
Eugene <134902746+H-Eugene@users.noreply.github.com>
2025-05-21 15:41:39 +0300

    comlete

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = []
 
-function Book(title, author, numberOfPages, isRead) {
+function Book(title, author, numberOfPages, status) {
   if (!new.target) {
     throw Error("Book instance should be created by 'new' operator");
   }
@@ -9,27 +9,92 @@ function Book(title, author, numberOfPages, isRead) {
   this.title = title;
   this.author = author;
   this.numberOfPages = numberOfPages;
-  this.isRead = isRead;
+  this.status = status;
 
   this.info = function () {
     let msg = (
       `${title} by ${author}, ${numberOfPages} pages, ` +
-      `${isRead ? "read" : "not read"}`
+      `${status ? "read" : "not read"}`
     );
 
     return msg;
   };
 }
 
-function addBookToLibrary(title, author, numberOfPages, isRead) {
-  myLibrary.push(new Book(title, author, numberOfPages, isRead));
+function addBookToLibrary(title, author, numberOfPages, status) {
+  myLibrary.push(new Book(title, author, numberOfPages, status));
+}
+
+function removeBookFromLibrary(id) {
+  const index = myLibrary.findIndex((el) => el["id"] === id);
+  if (index != -1) myLibrary.splice(index, 1);
+}
+
+function createBookNode(book) {
+    const node = document.createElement("tr");
+    let svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 23"><title>close-circle</title><path d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" /></svg>`
+    node.innerHTML = `
+        <td>${book.id}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.numberOfPages}</td>
+        <td>${book.status ? "Read" : "In Progress"}</td>
+        <td><button class="removeBtn">${svg}</button></td>
+    `;
+    return node;
+}
+
+function addBookToPage(book) {
+  let table = document.querySelector("tbody");
+  let node = createBookNode(book);
+  node.querySelector("button").addEventListener("click", 
+    e => {
+      const tr = e.currentTarget.parentElement.parentElement;
+      const id = tr.children[0].innerText;
+      removeBookFromLibrary(id);
+      tr.remove();
+    }
+  )
+  table.appendChild(node);
 }
 
 function displayBooks() {
+  for (book of myLibrary) {
+    addBookToPage(book);
+  }
+}
+
+function parseBook(form) {
   
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return null;
+  }
+
+  let title = document.querySelector('form [name="title"]').value;
+  let author = document.querySelector('form [name="author"]').value;
+  let numberOfPages = document.querySelector('form [name="numberOfPages"]').value;
+  let status = document.querySelector('form [name="status"]').checked;
+
+  return [title, author, numberOfPages, status]
 }
 
 addBookToLibrary("1984", "George Orwell", 328, true);
 addBookToLibrary("Dune", "Frank Herbert", 412, false);
 addBookToLibrary("The Little Prince", "Antoine de Saint-Exupéry", 96, true);
 addBookToLibrary("War and Peace", "Leo Tolstoy", 1225, false);
+
+displayBooks()
+
+document.querySelector("form button").addEventListener("click", 
+  function (e) {
+    const form = e.target.form;
+    const data = parseBook(form);
+    if (!data) return;
+
+    addBookToLibrary(...data);
+    addBookToPage(myLibrary.at(-1));
+    form.reset();
+    e.preventDefault();
+  }
+)
